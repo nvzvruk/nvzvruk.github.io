4206c3c63046e69c8bb47c76b427198d6252273c
Dmitriy Nazaruk <nazaruk44@gmail.com>
2017-05-10 18:18:37 +0300

    add fruitful

--- fruitful/gulpfile.js
+++ fruitful/gulpfile.js
@@ -3,14 +3,14 @@ var gulp         = require('gulp'),
      sass         = require('gulp-sass'),
      autoprefixer = require('autoprefixer'),
      browser      = require('browser-sync'),
-     sourcemaps   = require('gulp-sourcemaps');
+     sourcemaps   = require('gulp-sourcemaps'),
+    iconfont     = require("gulp-iconfont"),
+    consolidate  = require("gulp-consolidate");
  	
  gulp.task('build:sass', function () {
    return gulp.src('scss/**/*.scss')
          .pipe(sourcemaps.init())
-         .pipe(sass({
-               includePaths: ['node_modules/foundation-sites/scss', 'node_modules/motion-ui/src']
-           }).on('error', sass.logError))
+         .pipe(sass().on('error', sass.logError))
          .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
          .pipe(sourcemaps.write('.'))
          .pipe(gulp.dest('./css'))
@@ -25,6 +25,28 @@ var gulp         = require('gulp'),
          }
      });
  });
+
+gulp.task("build:icons", function() {
+   return gulp.src(["./assets/icons/*.svg"])//path to svg icons
+     .pipe(iconfont({
+       fontName: "myicons",
+       formats: ["ttf", "eot", "woff", "svg"],
+       centerHorizontally: true,
+       fixedWidth: true,
+       normalize: true
+     }))
+     .on("glyphs", function (glyphs) {
+
+       gulp.src("./assets/icons/util/*.scss") // Template for scss files
+           .pipe(consolidate("lodash", {
+               glyphs: glyphs,
+               fontName: "myicons",
+               fontPath: "../dist/fonts/"
+           }))
+           .pipe(gulp.dest("./assets/scss/icons/"));//generated scss files with classes
+     })
+     .pipe(gulp.dest("dist/fonts/"));//icon font destination
+});
 
  // Runs all of the above tasks and then waits for files to change
  gulp.task('default', ['serve'], function() {
