4428c7ab7b6263338edccfe4e5192b783571d6ba
Dmitriy Nazaruk <nazaruk44@gmail.com>
2017-08-10 21:48:24 +0300

    slider

--- Mogo/assets/js/main.js
+++ Mogo/assets/js/main.js
@@ -3,7 +3,12 @@ $(document).ready(function () {
         $(this).hasClass("is-active") ? $(this).removeClass('is-active') : $(this).addClass("is-active");
     });
     $('div.js-dropDown-btn').click(function () {
-        $('div.js-dropDown-links').hasClass("is-active") ? $('div.js-dropDown-links').removeClass('is-active') : $('div.js-dropDown-links').addClass("is-active"); 
+        $('div.js-dropDown-links').addClass("is-active");
+    });
+    $('div.js-dropDown-btn').click(function () {
+        if($('div.js-dropDown-links').hasClass("is-active")) {
+            $('div.js-dropDown-links').removeClass('is-active')
+        }
     });
     $('div.js-dropDown-links').mouseleave(function () {
         $(this).removeClass('is-active');
---
+++ Slider/script.js
@@ -0,0 +1,79 @@
+var navItem = document.querySelectorAll('.js-item');
+var slide = document.querySelector('.js-slide');
+var leftArr = document.querySelector('.js-left-arrow');
+var rightArr = document.querySelector('.js-right-arrow');
+var sliderWidth = document.querySelector('.slider');
+
+
+function clear(arr) {
+    arr.forEach(function (e) {
+        if (e.classList.contains('is-active')) {
+            e.classList.remove('is-active');
+        }
+    })
+}
+
+var index = 0;
+
+var start = setInterval(function () {
+    index++;
+    if (index < navItem.length) {
+        clear(navItem);
+        navItem[index].classList.add('is-active');
+        var w = slide.offsetWidth;
+        slide.style.marginLeft = -w * index + "px";
+    } else {
+        index = 0;
+        clear(navItem);
+        navItem[index].classList.add('is-active');
+        var w = slide.offsetWidth;
+        slide.style.marginLeft = -w * index + "px";
+    }
+}, 2000);
+
+navItem.forEach(function (e, i) {
+    e.addEventListener('click', function () {
+        index = i;
+        clear(navItem);
+        this.classList.add('is-active');
+        var w = slide.offsetWidth;
+        slide.style.marginLeft = -w * index + "px";
+        clearInterval(start);
+    })
+})
+
+
+rightArr.addEventListener('click', function () {
+    index++;
+    if (index < navItem.length) {
+        clear(navItem);
+        navItem[index].classList.add('is-active');
+        var w = slide.offsetWidth;
+        slide.style.marginLeft = -w * index + "px";
+    } else {
+        index = 0;
+        clear(navItem);
+        navItem[index].classList.add('is-active');
+        var w = slide.offsetWidth;
+        slide.style.marginLeft = -w * index + "px";
+    }
+    clearInterval(start);
+})
+
+
+leftArr.addEventListener('click', function () {
+    index--;
+    if (index >= 0) {
+        clear(navItem);
+        navItem[index].classList.add('is-active');
+        var w = slide.offsetWidth;
+        slide.style.marginLeft = -w * index + "px";
+    } else {
+        index = navItem.length - 1;
+        clear(navItem);
+        navItem[index].classList.add('is-active');
+        var w = slide.offsetWidth;
+        slide.style.marginLeft = -w * index + "px";
+    }
+    clearInterval(start);
+})
