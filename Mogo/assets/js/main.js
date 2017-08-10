1e5c30e7d2f5b7f1632e10b7e5d6e00cdd05c481
Dmitriy Nazaruk <nazaruk44@gmail.com>
2017-08-10 14:42:54 +0300

    3 blocks remake

--- Mogo/assets/js/main.js
+++ Mogo/assets/js/main.js
@@ -1,46 +1,11 @@
 $(document).ready(function () {
-    enableSelectBoxes();
-});
-
-function enableSelectBoxes() {
-    $('div.search-form__select').each(function () {
-        $(this).children('span.selected').html($(this).children('div.selectOptions').children('span.selectOption:first').html());
-        $(this).attr('value', $(this).children('div.selectOptions').children('span.selectOption:first').attr('value'));
-
-        $(this).children('span.selected,span.selectArrow').click(function () {
-            if ($(this).parent().children('div.selectOptions').css('display') == 'none') {
-                $(this).parent().children('div.selectOptions').css('display', 'block');
-            } else {
-                $(this).parent().children('div.selectOptions').css('display', 'none');
-            }
-        });
-
-        $(this).find('span.selectOption').click(function () {
-            $(this).parent().css('display', 'none');
-            $(this).closest('div.search-form__select').attr('value', $(this).attr('value'));
-            $(this).parent().siblings('span.selected').html($(this).html());
-        });
+    $('div.js_service_name').click(function () {
+        $(this).hasClass("is-active") ? $(this).removeClass('is-active') : $(this).addClass("is-active");
     });
-}
-
-
-$(document).ready(function () {
-    var outputSpan = $('#spanOutput');
-    var sliderDiv = $('#slider');
-    sliderDiv.slider({
-        range: true,
-        min: 0,
-        max: 1000000,
-        values: [200000, 500000],
-        slide: function (event, ui) {
-            outputSpan.html(ui.values[0] + ' - ' + ui.values[1] + ' Years');
-        },
-        stop: function (event, ui) {
-            $('#txtMinAge').val(ui.values[0]);
-            $('#txtMaxAge').val(ui.values[1]);
-        }
+    $('div.js-dropDown-btn').click(function () {
+        $('div.js-dropDown-links').hasClass("is-active") ? $('div.js-dropDown-links').removeClass('is-active') : $('div.js-dropDown-links').addClass("is-active"); 
     });
-    outputSpan.html(sliderDiv.slider('values', 0) + ' - ' + sliderDiv.slider('values', 1) + ' Years');
-    $('#txtMinAge').val(sliderDiv.slider('values', 0));
-    $('#txtMaxAge').val(sliderDiv.slider('values', 1));
+    $('div.js-dropDown-links').mouseleave(function () {
+        $(this).removeClass('is-active');
+    });
 });
