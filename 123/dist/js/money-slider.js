bf35b55f16caf734e8561948bb70ba080f3026d2
nvzvruk <nazaruk44@gmail.com>
2017-09-26 16:07:30 +0300

    new logo

---
+++ 123/dist/js/money-slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.money-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+});
